0ec9fdbd091478d63fa777af07e6bf54d3348c8b
Luca Angrisani <angrisani.luca@gmail.com>
2025-12-01 20:16:38 +0100

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -62,6 +62,12 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
